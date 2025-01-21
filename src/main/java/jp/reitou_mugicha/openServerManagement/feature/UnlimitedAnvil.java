6d2883d43ce0ce590a20e8ad56a7343918fec787
Your Name <your.email@example.com>
2025-01-21 20:55:37 +0900

    Remake UnlimitedAnvil

--- src/main/java/jp/reitou_mugicha/openServerManagement/feature/UnlimitedAnvil.java
+++ src/main/java/jp/reitou_mugicha/openServerManagement/feature/UnlimitedAnvil.java
@@ -18,6 +18,11 @@ import org.bukkit.inventory.meta.ItemMeta;
 import java.util.Arrays;
 
 public class UnlimitedAnvil implements Listener {
+    private final String GUIName = "改造金床";
+    private final int cost = 30;
+    private final int slot1Index = 11;
+    private final int slot2Index = 13;
+    private final int resultIndex = 15;
 
     private final Enchantment[] allowedEnchantments = {
             Enchantment.UNBREAKING,
@@ -34,117 +39,129 @@ public class UnlimitedAnvil implements Listener {
     };
 
     @EventHandler
-    public void onInventoryClick(InventoryClickEvent event) {
-        if (!(event.getWhoClicked() instanceof Player)) return;
-
-        Player player = (Player) event.getWhoClicked();
-
-        if (event.getView().getTitle().equals("Upgrade")) {
-            if (event.getClickedInventory() == null || event.getCurrentItem() == null) {
-                return;
-            }
-
-            if (event.getCurrentItem().getType() == Material.GRAY_STAINED_GLASS_PANE || event.getCurrentItem().getType() == Material.ANVIL)
-            {
-                event.setCancelled(true);
-            }
-
-            if (event.getSlot() == 13) {
-                Inventory inventory = event.getClickedInventory();
-
-                ItemStack slot1 = inventory.getItem(11);
-                ItemStack slot2 = inventory.getItem(15);
-
-                if (slot1 != null && slot2 != null) {
-                    if (slot1.getType() == Material.ENCHANTED_BOOK && slot2.getType() == Material.NETHERITE_INGOT)
-                    {
-                        if (hasAllowedEnchantment(slot1)) {
-                            if (getAllowedEnchantmentLevel(slot1) + 1 > 10) {
-                                player.sendMessage("§c最高エンチャントレベルに到達しました。");
-                                player.closeInventory();
-                                return;
-                            }
-
-                            ItemStack newEnchantedBook = new ItemStack(Material.ENCHANTED_BOOK);
-                            EnchantmentStorageMeta meta = (EnchantmentStorageMeta) newEnchantedBook.getItemMeta();
-                            meta.addStoredEnchant(getAllowedEnchantment(slot1), getAllowedEnchantmentLevel(slot1) + 1, true);
-                            newEnchantedBook.setItemMeta(meta);
-
-                            player.getInventory().addItem(newEnchantedBook);
-                            slot1.setAmount(slot1.getAmount() - 1);
-                            slot2.setAmount(slot2.getAmount() - 1);
-
-                            player.sendMessage("§a正常にアップグレードできました。");
-                            player.giveExpLevels(-10);
-                            player.closeInventory();
-                        }
-                    }
-                    else if ((Helpers.isTool(slot1) || Helpers.isArmor(slot1)) && slot2.getType() == Material.ENCHANTED_BOOK)
-                    {
-                        ItemStack newItem = new ItemStack(slot1.clone());
-                        ItemMeta meta = newItem.getItemMeta();
-                        EnchantmentStorageMeta bookMeta = (EnchantmentStorageMeta) slot2.getItemMeta();
-
-                        if (meta != null && bookMeta != null) {
-                            for (Enchantment enchant : bookMeta.getStoredEnchants().keySet()) {
-                                if (meta.hasEnchant(enchant)) {
-                                    player.sendMessage("§c" + enchant.getKey().getKey() + " はすでに付与されています。");
-                                    continue;
-                                }
-
-                                int level = bookMeta.getStoredEnchantLevel(enchant);
-                                meta.addEnchant(enchant, level, true);
-                            }
-
-                            newItem.setItemMeta(meta);
-
-                            player.getInventory().addItem(newItem);
-                            slot1.setAmount(slot1.getAmount() - 1);
-                            slot2.setAmount(slot2.getAmount() - 1);
-
-                            player.sendMessage("§a正常にエンチャントが付与されました。");
-                            player.giveExpLevels(-10);
-                            player.closeInventory();
-                        }
-                    }
-                }
-            } else {
-                event.setCancelled(false);
-            }
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getClickedBlock().getType() == Material.ANVIL) {
+            openCustomGUI(player);
         }
     }
 
     @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        Player player = event.getPlayer();
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getView().getTitle().equals(GUIName)) {
+            Player player = (Player) event.getWhoClicked();
+            ItemStack item = event.getCurrentItem();
+            if (item == null || item.getType() == Material.AIR) {
+                return;
+            }
 
-        if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (player.isSneaking() && player.getInventory().getItemInMainHand().getType() == Material.ANVIL) {
-                openCustomGUI(player);
-                event.setCancelled(true);
-            }
-            else if (event.getClickedBlock() != null && event.getClickedBlock().getType() == Material.ANVIL)
+            Inventory inventory = event.getInventory();
+            ItemStack slot1 = event.getInventory().getItem(slot1Index);
+            ItemStack slot2 = event.getInventory().getItem(slot2Index);
+
+            switch (event.getSlot())
             {
-                if (player.isSneaking())
-                {
-                    openCustomGUI(player);
-                    event.setCancelled(true);
-                }
+                case slot1Index:
+                case slot2Index:
+                    if (slot1 != null && slot2 != null) {
+                        if (slot1.getType() == Material.ENCHANTED_BOOK && slot2.getType() == Material.NETHERITE_INGOT)
+                        {
+                            if (hasAllowedEnchantment(slot1)) {
+                                if (checkHasEnoughExp(player, inventory))
+                                {
+                                    ItemStack result = slot1.clone();
+                                    EnchantmentStorageMeta resultMeta = (EnchantmentStorageMeta) result.getItemMeta();
+                                    resultMeta.addStoredEnchant(getAllowedEnchantment(slot1), getAllowedEnchantmentLevel(slot1) + 1, true);
+                                    result.setItemMeta(resultMeta);
+
+                                    inventory.setItem(resultIndex, result);
+                                }
+                            }
+                        } else if ((Helpers.isTool(slot1) || Helpers.isArmor(slot1)) && slot2.getType() == Material.ENCHANTED_BOOK)
+                        {
+                            if (hasAllowedEnchantment(slot2)) {
+                                if (checkHasEnoughExp(player, inventory))
+                                {
+                                    if (slot1.getEnchantments().keySet().stream().anyMatch(enchantment -> Arrays.asList(allowedEnchantments).contains(enchantment)) &&
+                                            slot1.getEnchantments().keySet().stream().anyMatch(enchantment -> Arrays.asList(allowedEnchantments).contains(enchantment) &&
+                                                    slot1.getEnchantmentLevel(getAllowedEnchantment(slot2)) >= getAllowedEnchantmentLevel(slot2)))
+                                    {
+                                        ItemStack resultItem = new ItemStack(Material.RED_CONCRETE);
+                                        ItemMeta resultMeta = resultItem.getItemMeta();
+                                        resultMeta.setDisplayName("エンチャントがかぶっているか、レベルが低いです。\nEnchantment is overlapping or level is low.");
+                                        resultItem.setItemMeta(resultMeta);
+                                        inventory.setItem(resultIndex, resultItem);
+                                        return;
+                                    }
+
+                                    ItemStack result = slot1.clone();
+                                    ItemMeta resultMeta = result.getItemMeta();
+                                    resultMeta.addEnchant(getAllowedEnchantment(slot2), getAllowedEnchantmentLevel(slot2), true);
+                                    result.setItemMeta(resultMeta);
+
+                                    inventory.setItem(resultIndex, result);
+                                }
+                            }
+                        }
+                    }
+                    break;
+                case resultIndex:
+                    if (item.getType() != Material.BARRIER && item.getType() != Material.RED_CONCRETE)
+                    {
+                        ItemStack resultItem = new ItemStack(Material.BARRIER);
+                        ItemMeta resultMeta = resultItem.getItemMeta();
+                        resultMeta.setDisplayName("アイテムをセットしてください | Set an item");
+                        resultItem.setItemMeta(resultMeta);
+
+                        player.getInventory().addItem(item);
+                        player.giveExpLevels(-cost);
+                        slot1.setAmount(slot1.getAmount() - 1);
+                        slot2.setAmount(slot2.getAmount() - 1);
+                        inventory.setItem(resultIndex, resultItem);
+                    }
+                    break;
+                default:
+                    if (event.getRawSlot() < 27)
+                    {
+                        event.setCancelled(true);
+                    }
+                    break;
             }
+
+
         }
     }
 
-    public void openCustomGUI(Player player) {
-        Inventory gui = Bukkit.createInventory(null, 27, "Upgrade");
+    public boolean checkHasEnoughExp(Player player, Inventory inventory) {
+        if (player.getExpToLevel() < cost) {
+            ItemStack resultItem = new ItemStack(Material.RED_CONCRETE);
+            ItemMeta resultMeta = resultItem.getItemMeta();
+            resultMeta.setDisplayName(String.format("経験値が足りません。| Not enough experience.\nコスト: %d | Cost: %d", cost, cost));
+            resultItem.setItemMeta(resultMeta);
+            inventory.setItem(resultIndex, resultItem);
+        }
+        return player.getExpToLevel() >= cost;
+    }
 
-        ItemStack filler = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
+    public void openCustomGUI(Player player) {
+        Inventory gui = Bukkit.createInventory(null, 27, GUIName);
+
+        ItemStack blank = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
+        ItemMeta blankMeta = blank.getItemMeta();
+        blankMeta.setDisplayName("無効 | Invalid");
+        blank.setItemMeta(blankMeta);
         for (int i = 0; i < 27; i++) {
-            gui.setItem(i, filler);
+            gui.setItem(i, blank);
         }
 
-        gui.setItem(11, null);
-        gui.setItem(15, null);
-        gui.setItem(13, new ItemStack(Material.ANVIL));
+        ItemStack resultItem = new ItemStack(Material.BARRIER);
+        ItemMeta resultMeta = resultItem.getItemMeta();
+        resultMeta.setDisplayName("アイテムをセットしてください | Set an item");
+        resultItem.setItemMeta(resultMeta);
+
+        gui.setItem(slot1Index, null);
+        gui.setItem(slot2Index, null);
+        gui.setItem(resultIndex, resultItem);
 
         player.openInventory(gui);
     }
@@ -195,8 +212,9 @@ public class UnlimitedAnvil implements Listener {
 
             Inventory inventory = event.getInventory();
 
-            ItemStack slot1 = inventory.getItem(11);
-            ItemStack slot2 = inventory.getItem(15);
+            ItemStack slot1 = inventory.getItem(slot1Index);
+            ItemStack slot2 = inventory.getItem(slot2Index);
+            ItemStack result = inventory.getItem(resultIndex);
 
             if (slot1 != null) {
                 player.getInventory().addItem(slot1);
@@ -204,6 +222,11 @@ public class UnlimitedAnvil implements Listener {
 
             if (slot2 != null) {
                 player.getInventory().addItem(slot2);
+            }
+
+            if (result != null && result.getType() != Material.BARRIER)
+            {
+                player.getInventory().addItem(result);
             }
         }
     }
